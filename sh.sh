a65832f600725ee5f4ced064538520c35f434356
danieliko2 <danielkohav2@gmail.com>
2022-09-08 15:41:39 +0300

    test commit

--- sh.sh
+++ sh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-major=$(git tag | awk -F \. {'print $1'})
-minor=$(git tag | awk -F \. {'print $2'})
-patch=$(git tag | awk -F \. {'print $3'})
+major=$(git tag | tail -1 | awk -F \. {'print $1'})
+minor=$(git tag | tail -1 | awk -F \. {'print $2'})
+patch=$(git tag | tail -1 | awk -F \. {'print $3'})
 
 echo $major
 echo $minor
