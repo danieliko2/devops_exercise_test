1ba4ebb382652dc8749dd8cc907b78d4ea25b02a
danieliko2 <danielkohav2@gmail.com>
2022-09-09 12:28:32 +0300

    Artifact upload working and versinoning complete

--- ss.sh
+++
@@ -1,5 +0,0 @@
-docker ps
-echo $(docker ps | grep myapptest | awk '{ print $1 }')
-containerId=$(docker ps | grep myapptest | awk '{ print $1 }')
-echo $(docker ps | grep ${containerId})
-# docker cp ${containerId}:/App/hello.txt ./Artifact
